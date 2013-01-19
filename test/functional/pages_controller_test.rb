063aacf128c6b6c3652bfe26b2c37ac9128bcaa7
andrea longhi <andrea@spaghetticode.it>
2013-01-19 18:51:07 +0100

    test pages_controller

--- test/functional/pages_controller_test.rb
+++ test/functional/pages_controller_test.rb
@@ -1,1 +1,36 @@
 require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test '#index renders if not logged in' do
+    get :index
+    assert_response :success
+  end
+
+  test '#index redirects to today page if logged in' do
+    sign_in :user, FactoryGirl.create(:user)
+    get :index
+    assert_redirected_to today_path
+  end
+
+  test '#rules redirects when not logged in' do
+    get :rules
+    assert_response :redirect
+  end
+
+  test '#rules renders when logged in' do
+    sign_in :user, FactoryGirl.create(:user)
+    get :rules
+    assert_response :success
+  end
+
+  test '#menu redirects when not logged in' do
+    get :menu
+    assert_response :redirect
+  end
+
+  test '#menu renders when logged in' do
+    sign_in :user, FactoryGirl.create(:user)
+    get :menu
+    assert_response :success
+  end
+end
