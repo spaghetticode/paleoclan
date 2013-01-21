5311b308653d569040dc44623a8d6d15e26b08e4
andrea longhi <andrea@spaghetticode.it>
2013-01-21 18:57:00 +0100

    fix admin/users_controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,26 @@ class Admin::UsersController < AdminController
   def index
     @users = User.all
   end
+
+  def ban
+    @user = User.find params[:user_id]
+    @user.ban
+    redirect_to admin_users_path, :notice => 'Utente bannato.'
+  end
+
+  def unban
+    @user = User.find params[:user_id]
+    if @user.unban
+      flash[:notice] = 'Utente sbannato.'
+    else
+      flash[:alert] = "L'utente ha raggiunto il limite di ban."
+    end
+    redirect_to admin_users_path
+  end
+
+  def destroy
+    @user = User.find params[:user_id]
+    @user.destroy
+    redirect_to admin_users_path, :notice => 'Utente cancellato.'
+  end
 end
--- test/functional/admin/users_controller_test.rb
+++ test/functional/admin/users_controller_test.rb
@@ -17,4 +17,57 @@ class Admin::UsersControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test '#ban redirects' do
+    user = FactoryGirl.build :user
+    user.stubs(:ban).returns(true)
+    User.stubs(:find).returns(user)
+    sign_in_admin
+    put :ban, :user_id => 42
+    assert_redirected_to admin_users_path
+  end
+
+  # test '#ban bans the user' do
+  #   user = FactoryGirl.build :user
+  #   user.expects(:ban).returns(true)
+  #   User.stubs(:find).returns(user)
+  #   sign_in_admin
+  #   put :ban, :user_id => 42
+  # end
+
+  test '#unban redirects' do
+    user = FactoryGirl.build :user
+    user.stubs(:unban).returns(true)
+    User.stubs(:find).returns(user)
+    sign_in_admin
+    put :unban, :user_id => 42
+    assert_redirected_to admin_users_path
+  end
+
+  test '#unban when successful has a notice message' do
+    user = FactoryGirl.build :user
+    User.stubs(:find).returns(user)
+    user.stubs(:unban).returns(true)
+    sign_in_admin
+    put :unban, :user_id => 42
+    assert flash[:notice].present?
+  end
+
+  test '#unban when failing has a alert message' do
+    user = FactoryGirl.build :user
+    User.stubs(:find).returns(user)
+    user.stubs(:unban).returns(false)
+    sign_in_admin
+    put :unban, :user_id => 42
+    assert flash[:alert].present?
+  end
+
+  test '#destroy redirects' do
+    user = FactoryGirl.build :user
+    user.stubs(:destroy).returns(true)
+    User.stubs(:find).returns(user)
+    sign_in_admin
+    delete :destroy, :user_id => 42
+    assert_redirected_to admin_users_path
+  end
 end
