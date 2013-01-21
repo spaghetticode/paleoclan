83b73a1019f6dfbc7affaa5660d7ce40eebeb0c5
andrea longhi <andrea@spaghetticode.it>
2013-01-21 10:40:29 +0100

    fix slots_controller

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -5,7 +5,7 @@ class SlotsController < AuthenticatedController
   def create
     redirect_to today_roulette_path if roulette?
     if reservations_time?
-      @slot = new(:user => current_user, :day => today)
+      @slot = Slot.new(:user => current_user, :day => today)
       if @slot.save
         redirect_to today_path, :notice => 'Ti sei prenotato per il pranzo di oggi' and return
       end
