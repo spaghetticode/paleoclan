3c07ecb79b32facf09a8f96c90dc1f8d36eb671c
andrea longhi <andrea@spaghetticode.it>
2013-01-22 10:31:41 +0100

    bad way to parse config

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -7,7 +7,8 @@ class Admin::SettingsController < AdminController
 
   def update
     @settings = Settings.instance
-    if @settings.update_attributes params[:settings]
+    # TODO this is evil!
+    if @settings.update_attributes :data => Hashifier.new(params[:settings][:data])
       redirect_to edit_admin_settings_path, :notice => 'Configurazione aggiornata.'
     else
       flash[:alert] = 'Non è possibile aggiornare la configurazione.'
