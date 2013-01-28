3cdfcfde8f13056aeb1114684919352517638fa9
andrea longhi <andrea@spaghetticode.it>
2013-01-28 11:27:07 +0100

    remove Hashifier reference

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -8,7 +8,7 @@ class Admin::SettingsController < AdminController
   def update
     @settings = Settings.instance
     # TODO this is evil!
-    if @settings.update_attributes :data => Hashifier.new(params[:settings][:data])
+    if @settings.update_attributes :data => eval(params[:settings][:data])
       redirect_to edit_admin_settings_path, :notice => 'Configurazione aggiornata.'
     else
       flash[:alert] = 'Non è possibile aggiornare la configurazione.'
