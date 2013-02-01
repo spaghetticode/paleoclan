1f0268c280d2d484938757faaa51a10a1b366381
andrea longhi <andrea@spaghetticode.it>
2013-02-01 08:53:24 +0100

    redirect to ratings#new if lunch is over and user can rate

--- app/controllers/todays_controller.rb
+++ app/controllers/todays_controller.rb
@@ -1,4 +1,5 @@
 class TodaysController < AuthenticatedController
+  before_filter :redirect_to_new_rating_if_user_can_rate
   def show
     redirect_to roulette_today_path if roulette?
   end
@@ -12,5 +13,11 @@ class TodaysController < AuthenticatedController
 
   def extract_winners
     today.extract_winners if !today.full? and hour >= Settings.close_hour
+  end
+
+  def redirect_to_new_rating_if_user_can_rate
+    if rating_allowed? and hour > Settings.close_hour
+      redirect_to new_rating_path
+    end
   end
 end
